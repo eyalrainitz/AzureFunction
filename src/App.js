3ab1daf686b26f89ae7ce0409574c1309bf08088
Eyal Rainitz <eyalr@sela.co.il>
2022-06-27 19:27:01 +0300

    add

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ const App = () => {
   const [blob, setblob] = useState("");
   const [input, setinput] = useState("");
   const [ResdData, setResData] = useState([]);
-
+  const functionappNode = "selafunctionpocnode"
+  const functionappPython = "selafunctionpython"
+  
   const SendName = async (name) => {
     if (name !== null || name !== "" || name !== " ") {
       await axios
-        .post("https://selafunctionpocnode.azurewebsites.net/api/BlobPost", {
+        .post(`https://${functionappNode}.azurewebsites.net/api/BlobPost`, {
           name,
         })
         .then((res) => {
@@ -22,12 +24,12 @@ const App = () => {
   const [cosmosGet, setcosmosGet] = useState([]);
   const cosmosGetnode = async () => {
     await axios
-      .get("https://selafunctionpocnode.azurewebsites.net/api/CosmosGet")
+      .get(`https://${functionappNode}.azurewebsites.net/api/CosmosGet`)
       .then((res) => setcosmosGet([...res.data]));
   };
   const CosmosPostNode = async (name) => {
     await axios
-      .post("https://selafunctionpocnode.azurewebsites.net/api/CosmosPost", {
+      .post(`https://${functionappNode}.azurewebsites.net/api/CosmosPost`, {
         name,
       })
       .then((res) => setcosmosGet([...res.data]));
@@ -37,7 +39,7 @@ const App = () => {
   const GetName = async (name) => {
     await axios
       .get(
-        `https://selafunctionpocnode.azurewebsites.net/api/BlobGet/?name=${name}`
+        `https://${functionappNode}.azurewebsites.net/api/BlobGet/?name=${name}`
       )
       .then((res) => setUserData(res.data))
       .then((jsondata) => {
@@ -48,23 +50,22 @@ const App = () => {
   const cosmosGetData = cosmosGet.map((user, index) => (
     <div key={index}>{user}</div>
   ));
-
-  // const blobdata =
+  const [cGet, setcGet] = useState("");
   /////////////////////////////////// python
-  const [cosmosGetPython, setcosmosGetPython] = useState([]);
+  const [cosmosGetPython, setcosmosGetPython] = useState('');
   // Get number of items in cosmosdb
-  const CosmosGetPython = async () => {
+  const CosmosGetPython = async (number) => {
     await axios
-      .get(`https://selafunctionpython.azurewebsites.net/api/CosmosGet?`)
+      .post(`https://${functionappPython}.azurewebsites.net/api/CosmosGet`,{number})
       .then((res) => setcosmosGetPython(JSON.parse(JSON.stringify(res.data))));
   };
   const [cosmosPostPython, setcosmosPostPython] = useState([]);
   // Post User inside DB
   const CosmosPostPython = (name) => {
     axios
-      .post(`https://selafunctionpython.azurewebsites.net/api/CosomosPost`, {
-        name,
-      })
+      .post(`https://${functionappPython}.azurewebsites.net/api/CosmosPost`, 
+        {name},
+      )
       .then((res) => setcosmosPostPython(JSON.parse(JSON.stringify(res.data))));
   };
   const [HttpBlobGPython, setHttpBlobython] = useState([]);
@@ -72,7 +73,7 @@ const App = () => {
   const HttpBlobGetPython = (name) => {
     axios
       .get(
-        `https://selafunctionpython.azurewebsites.net/api/HttpBlob/?name=${name}`
+        `https://${functionappPython}.azurewebsites.net/api/HttpBlob/?name=${name}`
       )
       .then((res) => setHttpBlobython(res.data));
   };
@@ -80,18 +81,53 @@ const App = () => {
   // http post data
   const HttpBlobPostPython = (name) => {
     axios
-      .post(`https://selafunctionpython.azurewebsites.net/api/HttpBlobPost`, {
+      .post(`https://${functionappPython}.azurewebsites.net/api/HttpBlobPost`, {
         name,
       })
       .then((res) => setHttpBlobPostPython(res.data));
   };
 
+  ////////////////////////////////////////
+
+  // cosmos python delete all items
+  const HttpCosmosDeleteAll = () => {
+    axios
+      .get(
+        `https://${functionappPython}.azurewebsites.net/api/DeleteItemsInCosmosDB`
+      )
+      .then((res) => console.log(res));
+  };
+
+  // cosmos python delete by name
+  const HttpCosmosDeleteUser = (name) => {
+    axios
+      .post(
+        `https://${functionappPython}.azurewebsites.net/api/CosmosDelete`,
+        name
+      )
+      .then((res) => console.log(res));
+  };
+  ////////////////////////////////////////////////////
+
+  // delete blob
+  const HttpDeleteBlob = (name) =>{
+    axios.post(`https://${functionappPython}.azurewebsites.net/api/blobstorageDelete`)
+  }
   ////////////////////////////////////////////////////
 
   useEffect(() => {
     cosmosGetnode();
-    CosmosGetPython();
   }, []);
+
+
+  
+  const handleChangeGet = (e) =>{
+    setcGet(e.target.value)
+  }
+  const handleSubmitGet = (e) => {
+    e.preventDefault();
+    CosmosGetPython(cGet)
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -102,7 +138,7 @@ const App = () => {
   const handleChange = (e) => {
     setinput(e.target.value);
   };
-
+  ///////////////////
   const handleSubmitblob = (e) => {
     e.preventDefault();
     GetName(blob);
@@ -110,16 +146,17 @@ const App = () => {
   const handleChangeblob = (e) => {
     setblob(e.target.value);
   };
-
-  const [handle, sethandle] = useState([]);
+  /////////////////////
+  const [handle, sethandle] = useState("");
   const handleChangeCosmosPython = (e) => {
     sethandle(e.target.value);
   };
   const handleSubmitPython = (e) => {
     e.preventDefault();
     CosmosPostPython(handle);
-    window.location.reload();
+    // window.location.reload();
   };
+  /////////////////
   const handleSubmitPythonblob = (e) => {
     e.preventDefault();
     HttpBlobGetPython(handleblob);
@@ -128,6 +165,7 @@ const App = () => {
   const handleChangePythonblob = (e) => {
     sethandleblob(e.target.value);
   };
+  ////////////////
   const [handleblobpost, sethandleblobpost] = useState([]);
   const handleChangePythonblobpost = (e) => {
     sethandleblobpost(e.target.value);
@@ -136,6 +174,26 @@ const App = () => {
     e.preventDefault();
     HttpBlobPostPython(handleblobpost);
   };
+  ////////////////
+  const [handlecosmosdelete, sethandlecosmosdelete] = useState([]);
+  const handleChangehandlecosmosdelete = (e) => {
+    sethandlecosmosdelete(e.target.value);
+  };
+  const handleSubmithandlecosmosdelete = (e) => {
+    e.preventDefault();
+    HttpCosmosDeleteUser(handlecosmosdelete);
+  };
+  ////////////////
+  const [handleblobdelete, sethandleblobdelete] = useState([]);
+  const handleChangehandleblobdelete = (e) => {
+    sethandleblobdelete(e.target.value);
+  };
+  const handleSubmithandleblobdelete = (e) => {
+    e.preventDefault();
+    HttpCosmosDeleteUser(handleblobdelete);
+  };
+  ////////////////
+  
   return (
     <div className="App">
       <div>Node Functions</div>
@@ -160,9 +218,18 @@ const App = () => {
       <br></br>
       <div>Python Function </div>
       <div>
-        <label>Get Count of items in cosmos db</label>
+        <label>Get by id name of cosmosdb item</label>
+        <br></br>
+        <br></br>
+        <div>
+        <form onSubmit={handleSubmitGet}>
+        <input type="text" onChange={handleChangeGet}></input>
+        <input type="submit"></input>
+      </form>
+        </div>
         <div>{cosmosGetPython}</div>
       </div>
+      <br></br>
       <form onSubmit={handleSubmitPython}>
         <label>post cosmos </label>
         <br></br>
@@ -191,6 +258,29 @@ const App = () => {
           <input type="submit"></input>
         </form>
         {HttpBlobPPython}
+      </div>
+      <div>
+        <form onSubmit={handleSubmithandlecosmosdelete}>
+          <label>delete item by name </label>
+          <br></br>
+          <input type="text" onChange={handleChangehandlecosmosdelete}></input>
+          <br></br>
+          <input type="submit"></input>
+        </form>
+      </div>
+      <div>
+        <button onClick={HttpCosmosDeleteAll}>
+          Click for Delete all Items in Cosmos
+        </button>
+      </div>
+      <div>
+        <form onSubmit={handleSubmithandleblobdelete}>
+          <label>delete blob by name </label>
+          <br></br>
+          <input type="text" onChange={handleChangehandleblobdelete}></input>
+          <br></br>
+          <input type="submit"></input>
+        </form>
       </div>
     </div>
   );
